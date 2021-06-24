792bf4f1cdd620c7d1cb55ebe863ceaffc3152d3
Russell Rockwood <russell.rockwood@gmail.com>
2021-06-24 08:52:02 -0500

    NoteEdit functionality

--- ElevenNote.WebMVC/Controllers/NoteController.cs
+++ ElevenNote.WebMVC/Controllers/NoteController.cs
@@ -62,6 +62,21 @@ namespace ElevenNote.WebMVC.Controllers
 
             return View(model);
         }
+
+        public ActionResult Edit(int id)
+        {
+            var service = CreateNoteService();
+            var detail = service.GetNoteById(id);
+            var model =
+                new NoteEdit
+                {
+                    NoteId = detail.NoteId,
+                    Title = detail.Title,
+                    Content = detail.Content
+                };
+
+            return View(model);
+        }
        
     }
 }
